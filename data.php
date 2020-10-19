2c8fdad7a0019d980a1eda0eca1fec842ca1b6d2
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2020-10-19 10:49:30 +0200

    Included files to project and build two functions in function.php

---
+++ data.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+$users = [
+            'id' => '',
+            'username' => '',
+            'password' => '',
+            'full_name' => '',
+            'date_joined' => '',
+            'is_admin' => false
+];
+
+$news = [
+            'id' => '',
+            'title' => '',
+            'story' => '',
+            'date' => '',
+            'author' => ''
+];
